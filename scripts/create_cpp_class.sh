6c4a1df8a8eb22be9735015acf6b565340dc02d6
Beniamin <106469071+redve-dev@users.noreply.github.com>
2022-12-13 19:14:54 +0100

    init

---
+++ scripts/create_cpp_class.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+CLASS_NAME=$1
+
+if [[ -d $CLASS_NAME ]]; then
+	echo "The directory $CLASS_NAME already exist. Terminating"
+	exit 1
+fi
+
+mkdir $CLASS_NAME
+cp ../templates/cpp/class/* $CLASS_NAME/
+cd $CLASS_NAME
+
+# keep extension of each file, but change its actual name
+for file in `ls`
+do
+	NEW_FILENAME=`echo $file | sed -e "s/MY_CLASS/$CLASS_NAME/g"`
+	mv $file $NEW_FILENAME
+	sed -i -e "s/MY_CLASS/$CLASS_NAME/g" $NEW_FILENAME
+done
