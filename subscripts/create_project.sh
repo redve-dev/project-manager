6b1fc578fb558bc0d8cd17c39d5c23dc3a1b42e9
Beniamin <106469071+redve-dev@users.noreply.github.com>
2022-12-13 19:28:45 +0100

    little refactor

--- subscripts/create_project.sh
+++ subscripts/create_project.sh
@@ -4,8 +4,7 @@ function CreatePythonProject(){
 	echo -n "Enter project name: "
 	read PROJECT_NAME
 	if [[ ! -d $PROJECT_NAME ]]; then
-		cp -r $TEMPLATES_DIRECTORY/python/new_project $PROJECT_NAME
-		break
+		cp -r $TEMPLATES/python/new_project $PROJECT_NAME
 	else
 		echo "The directory already exist. Aborting"
 	fi
@@ -15,7 +14,7 @@ function CreateCPProject(){
 	echo -n "Enter project name: "
 	read PROJECT_NAME
 	if [[ ! -d $PROJECT_NAME ]]; then
-		cp -r $TEMPLATES_DIRECTORY/cpp/new_project $PROJECT_NAME
+		cp -r $TEMPLATES/cpp/new_project $PROJECT_NAME
 		cd $PROJECT_NAME
 		sed -i -e "s/MY_PROJECT/$PROJECT_NAME/g" meson.build
 		echo -n "Enter executable name: "
@@ -30,36 +29,36 @@ function CreateCPProject(){
 	fi
 }
 
-PS3='Select language: '
-CHANGE_DIRECTORY_AT_THE_END=true
-select opt in python cpp Quit
-do
-	case $opt in
-		"python")
-			CreatePythonProject
-			;;
-		"cpp")
-			CreateCPProject
-			;;
-		"Quit")
-			exit
-			break
-			;;
-		*)
-			echo "Unrecognized option. Please choose again"
-			;;
-	esac
-done
+function main(){
+	PS3='Select language: '
+	CHANGE_DIRECTORY_AT_THE_END=true
+	select opt in python cpp Quit
+	do
+		case $opt in
+			"python")
+				CreatePythonProject
+				;;
+			"cpp")
+				CreateCPProject
+				;;
+			"Quit")
+				exit
+				break
+				;;
+			*)
+				echo "Unrecognized option. Please choose again"
+				;;
+		esac
+	done
 
-if $CHANGE_DIRECTORY_AT_THE_END
-then
-	cd $PROJECT_NAME
-fi
+	if $CHANGE_DIRECTORY_AT_THE_END
+	then
+		cd $PROJECT_NAME
+	fi
 
-echo "# $PROJECT_NAME\n" > README.md
-git init
-mv gitignore .gitignore # move invalid template to actual .gitignore
-git add .
-git commit -m "init"
-
-			echo "here"
+	echo "# $PROJECT_NAME\n" > README.md
+	git init
+	mv gitignore .gitignore # move invalid template to actual .gitignore
+	git add .
+	git commit -m "init"
+}
